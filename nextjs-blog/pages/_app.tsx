33cc023be445ab6a0f8a10d1ca8058176a9d5327
kwam1na <kwamina.0x00@gmail.com>
2022-09-23 18:57:06 -0400

    add project

---
+++ nextjs-blog/pages/_app.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "../styles/global.css";
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ nextjs-blog/pages/posts/first-post.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+const FirstPost = () => {
+  return <></>;
+};
+
+export default FirstPost;
